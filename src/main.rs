de2e71479fefcfb3571a57634c63b03c6ce0540a
CG-SS <criguiss@uol.com.br>
2022-08-20 21:00:26 -0300

    Adding tide

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use tide::Request;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/health").get(get_health);
+
+    app.listen("127.0.0.1:8080").await?;
+
+    Ok(())
+}
+
+async fn get_health(mut _req: Request<()>) -> tide::Result {
+    Ok(format!("Up and running!").into())
 }
